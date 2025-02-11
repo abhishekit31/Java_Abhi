f91556eb095f7dbb44673a8fe1ace997d446f231
abhishek <abhishekit31@gmail.com>
2025-02-11 14:06:37 +0530

    initial commit

---
+++ Method.java
@@ -0,0 +1,24 @@
+// Method in java is a function which is used to behavior of an object.
+class Computer {
+    public void playMusic() {
+        System.out.println("Music Playing...");
+    }
+
+    public String getMeAPen(int cost) {
+
+        if (cost >= 10) {
+            return "pen";
+        }
+        return "Nothing find at this cost !";
+    }
+}
+
+public class Method {
+    public static void main(String[] args) {
+        Computer obj = new Computer();
+
+        obj.playMusic();
+        String str = obj.getMeAPen(2);
+        System.out.println(str);
+    }
+}
---
+++ MethodOverloading.java
@@ -0,0 +1,36 @@
+// Method overloading in java is a mechanism that provide us to crate a same method
+// inside the class with different parameter is known as method overloading.
+
+class Calculator {
+
+    int num = 10; // this is the instance variable and it is stored in heap memory.
+
+    public int add(int n1, int n2) {
+        // here n1 and n2 is the local variable becasue it is created iside the add
+        // method.
+        // here the n1 and n2 is stored in add stack
+        System.out.println(num);
+        return n1 + n2;
+    }
+
+    public int add(int n1, int n2, int n3) {
+        return n1 + n2 + n3;
+    }
+}
+
+public class MethodOverloading {
+
+    public static void main(String[] args) {
+
+        Calculator obj = new Calculator();
+
+        int data = 20; // the data is stored in the main stack
+        System.out.println(data);
+
+        int res = obj.add(10, 15);
+        System.out.println("The additionTwo is : " + res);
+
+        int res1 = obj.add(10, 15, 20);
+        System.out.println("The additionThree is : " + res1);
+    }
+}
