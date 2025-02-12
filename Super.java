6af72bcf7381629e91339b0e19915a6a27864436
abhishek <abhishekit31@gmail.com>
2025-02-12 23:39:00 +0530

    adding tha super class

---
+++ Super.java
@@ -0,0 +1,30 @@
+class A {
+    public A() {
+        super();
+        System.out.println("In A");
+    }
+
+    public A(int n) {
+        super();
+        System.out.println("In A int");
+    }
+}
+
+class B extends A {
+    public B() {
+        super();
+        System.out.println("In B");
+    }
+
+    public B(int n) {
+        super(n);
+        System.out.println("In B int");
+    }
+}
+
+public class Super {
+    public static void main(String[] args) {
+
+        B obj = new B(10);
+    }
+}
