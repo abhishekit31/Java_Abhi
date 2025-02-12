10eb0a8feeb94eda5a0ea4b31b51c0d6a391411e
abhishek <abhishekit31@gmail.com>
2025-02-12 13:54:08 +0530

    adding the static block class

---
+++ StaticBlock.java
@@ -0,0 +1,44 @@
+class Mobile {
+    static String name;
+    int price;
+    String brand;
+
+    // constructor
+    public Mobile() {
+        brand = "";
+        price = 200;
+        System.out.println("inside the constrcutor");
+    }
+
+    // static block - static block is excecuted only once no matter how many objects
+    // you have craeted and it is called first at the time of class loading by class
+    // loader
+    static {
+        name = "phoneeeee";
+        System.out.println("inside the satic block");
+    }
+
+    public void show() {
+        System.out.println(name + ":" + price + ":" + brand);
+    }
+}
+
+public class StaticBlock {
+    public static void main(String[] args) {
+
+        Mobile obj1 = new Mobile();
+        Mobile.name = "Smart Phone";
+        obj1.price = 1500;
+        obj1.brand = "Apple";
+
+        Mobile obj2 = new Mobile();
+        Mobile.name = "Smart Phone";
+        obj2.price = 1000;
+        obj2.brand = "Samsung";
+
+        // Mobile.name = "Phone";
+
+        obj1.show();
+        obj2.show();
+    }
+}
