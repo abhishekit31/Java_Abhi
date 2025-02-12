0caf0c50eb7d6543688f710309a2c2c35cb08332
abhishek <abhishekit31@gmail.com>
2025-02-12 23:05:59 +0530

    adding inheritance

---
+++ Inheritance/AdvCalc.java
@@ -0,0 +1,13 @@
+package Inheritance;
+
+public class AdvCalc extends Calculator {
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public int division(int a, int b) {
+        return a / b;
+    }
+
+}
